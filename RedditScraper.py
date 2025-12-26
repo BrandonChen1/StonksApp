02ba2b85a8b86e91cfec32c0ed417b1f8b92c5fe
Brandon Chen <brandonchen2045@gmail.com>
2025-12-26 18:07:47 -0500

    Start finding titles, text and comments per post

--- RedditScraper.py
+++ RedditScraper.py
@@ -21,19 +21,62 @@ def scrapeSubreddit(subreddit, limit=25):
     for post in data["data"]["children"]:
         p = post["data"]
         posts.append({
+            "id": p["id"],
             "title": p["title"],
-            "score": p["score"],
-            "comments": p["num_comments"],
-            "url": "reddit.com" + p["permalink"]
+            "body": p.get("selftext", ""),
+            "comments": p["num_comments"]
         })
 
     return posts
 
+# Scrape the tickers for a post
+def scrapePost(post):
+    print(post)
+    title = post['title']
+    body = post['body']
+    comments = scrapeComments(post['id'])
 
-# For a given post, find all the potential ticker symbols for the post
-def scrapePost(url):
-    tickers = []
-    return tickers
+
+# For a given post, find all the potential ticker symbols for the comments
+def scrapeComments(postId):
+    url = f"https://www.reddit.com/comments/{postId}.json"
+    params = {
+        "limit": 10,
+        "depth": 10,
+        "sort": "top"
+    }
+
+    response = requests.get(url, headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+    data = response.json()
+    comments = []
+
+    def parse_comment_tree(children, depth):
+        if depth == 2:
+            return
+        for child in children:
+            kind = child.get("kind")
+            data = child.get("data", {})
+
+            # Skip "more" placeholders
+            if kind != "t1":
+                continue
+
+            comments.append({
+                "comment_id": data["id"],
+                "body": data.get("body", ""),
+                "score": data["score"],
+                "created_utc": data["created_utc"],
+                "parent_id": data["parent_id"]
+            })
+
+            # Recursively parse replies
+            replies = data.get("replies")
+            if isinstance(replies, dict):
+                parse_comment_tree(replies["data"]["children"], depth+1)
+
+    parse_comment_tree(data[1]["data"]["children"], 0)
+    return comments
 
 # Find the most frequently mentioned ticker symbols given a list of potential symbols
 def countTickers(tickers):
@@ -48,8 +91,10 @@ def validateTickers(tickers):
 
 def main():
     subreddits = ["wallstreetbets"]
-    # print(scrapeSubreddit(subreddits[0]))
-    print(validateTickers(["AAPL"]))
+    posts = scrapeSubreddit(subreddits[0])
+    
+    scrapePost(posts[0])
+
     
 ## EXECUTE
 main()
