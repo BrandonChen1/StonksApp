4d80a9d2e899257d688b3a24a45e18015ee6ec23
Brandon Chen <brandonchen2045@gmail.com>
2025-12-26 16:41:31 -0500

    start scraping subreddits'

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,7 +1,30 @@
 import requests
 
-url = "http://reddit.com"
-response = requests.get(url)
+subreddits = ["wallstreetbets"]
+HEADERS = {
+    "User-Agent": "python:subreddit.scraper:v1.0 (by u/anonymous)"
+}
 
-print(response.status_code)
-print(response.text[:500])
+
+def scrape_subreddit(subreddit, limit=25):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"limit": limit}
+
+    response = requests.get(url, headers=HEADERS, params=params, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    posts = []
+    for post in data["data"]["children"]:
+        p = post["data"]
+        posts.append({
+            "title": p["title"],
+            "score": p["score"],
+            "comments": p["num_comments"],
+            "url": "reddit.com" + p["permalink"]
+        })
+
+    return posts
+
+print(scrape_subreddit(subreddits[0]))
