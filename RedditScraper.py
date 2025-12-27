d8dade834bae947fd2a6bc63b18d122fc6d02901
Brandon Chen <brandonchen2045@gmail.com>
2025-12-26 22:09:53 -0500

    start validating ticker symbols

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,14 +1,15 @@
 import requests
 from polygon import RESTClient
+import time
 
 HEADERS = {
-    "User-Agent": "python:subreddit.scraper:v1.0 (by u/anonymous)"
+    "User-Agent": "tester 0.1"
 }
 client = RESTClient(api_key="lIBofdm9q9wDCLfDg1lsqllOrpeYfC4l")
 
 
 # Scrape the hot posts for a given subreddit
-def scrapeSubreddit(subreddit, limit=25):
+def scrapeSubreddit(subreddit, limit=10):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {"limit": limit}
 
@@ -77,16 +78,66 @@ def scrapeComments(postId):
     parse_comment_tree(data[1]["data"]["children"], 0)
     return comments
 
+# Find all the potential tickers
+def findAllTickers(postData):
+    allTickers = []
+
+    # Title
+    allTickers.extend(findTicker(postData["title"]))
+
+    # Post
+    allTickers.extend(findTicker(postData["body"]))
+
+    # Comments
+    for comment in postData["comments"]:
+        allTickers.extend(findTicker(comment["body"]))
+    return allTickers
+
+
+# Get all potential tickers given a string
+def findTicker(string):
+
+    tickers = []
+    words = string.split()
+    for word in words:
+        if not word.isupper() or not word.isalpha():
+            continue
+        else:
+            tickers.append(word)
+    return tickers
+
 # Find the most frequently mentioned ticker symbols given a list of potential symbols
 def countTickers(tickers):
     return tickers
 
 # Validate that these are real tickers by querying https://massive.com/dashboard/keys
 # api key lIBofdm9q9wDCLfDg1lsqllOrpeYfC4l
-def validateTickers(tickers):
-    details = client.get_ticker_details(tickers[0])
-    return details
+# 5 requests per minute...
+def filterTickers(tickers, seen):
+    seen = {} if seen == {} else seen
+    filteredTickers = []
+    for ticker in tickers:
+        if ticker in seen:
+            if seen[ticker]:
+                filteredTickers.append(ticker)
+            continue
+        if len(ticker) > 5:
+            continue
+        try:
+            client.get_ticker_details(ticker)
+            filteredTickers.append(ticker)
+            seen[ticker] = 1
+        except Exception as e:
+            if "429" in e.args[0]:
+                print("hit rate limit, sleeping for 1 minute")
+                time.sleep(61)
+            seen[ticker] = 0
+            print("Could not find ticker symbol: " + ticker)
 
+    return {
+        "validTickers": tickers,
+        "seen": seen
+    }
 
 def main():
     subreddits = ["wallstreetbets"]
@@ -96,13 +147,20 @@ def main():
     postsData = []
     for post in posts:
         data = scrapePost(post)
-        print(data)
         postsData.append(data)
+        
+    tickers = []
 
+    for postData in postsData:
+        tickers.extend(findAllTickers(postData))
+    print(tickers)
 
     
-
-
+def test():
+    tickers = ['WSB', 'WSB', 'OP', 'I', 'JP', 'I', 'JP', 'I', 'LOOK', 'I', 'FINALLY', 'MADE', 'A', 'I', 'I', 'SLV', 'I', 'I', 'SLV', 'I', 'OF', 'YOU', 'WATCHED', 'SLV', 'RIP', 'ARE', 'BEARISH', 'BECAUSE', 'YOU', 'SLV', 'I', 'SLV', 'SLV', 'JP', 'I', 'WSB', 'I', 'XD', 'I', 'I', 'WSB', 'I', 'I', 'I', 'I', 'I', 'DID', 'I', 'I', 'I', 'I', 'I', 'I', 'YOY', 'WSB', 'I', 'I', 'I', 'WSB', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'AI', 'WSB', 'I', 'AI', 'CTO', 'I', 'I', 'I', 'I', 'CTO', 'STEM', 'I', 'LLM', 'AI', 'SDE', 'I', 'A', 'I', 'I', 'I', 'I', 'WSB', 'OP', 'SPY', 'I', 'ROTH', 'WSB', 'I', 'I', 'OP', 'SPY', 'YTD', 'TIL', 'YTD', 'GLD', 'WSB', 'I', 'I', 'I', 'I', 'WSB', 'I', 'SILJ', 'SLV', 'SLV', 'GLD', 'GLD']
+    filtered = filterTickers(tickers, {})
+    print(filtered)
     
 ## EXECUTE
-main()
+# main()
+test()
